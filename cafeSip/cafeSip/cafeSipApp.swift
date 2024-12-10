6ea2549dbb8270fe584bf3bbfb5d6d5faeda84a3
morgan4563 <morganpark4563@gmail.com>
2024-12-11 06:27:14 +0900

    feat: cafeSip 프로젝트 파일 생성

---
+++ cafeSip/cafeSip/cafeSipApp.swift
@@ -0,0 +1,17 @@
+//
+//  cafeSipApp.swift
+//  cafeSip
+//
+//  Created by hyunMac on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct cafeSipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
