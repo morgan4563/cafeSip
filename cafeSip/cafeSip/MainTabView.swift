b35c4be6143de0525c48e903901e89fd7d51b8cb
morgan4563 <morganpark4563@gmail.com>
2024-12-11 06:47:48 +0900

    feat: MainTabView 설정 , HomeView 연결

---
+++ cafeSip/cafeSip/MainTabView.swift
@@ -0,0 +1,41 @@
+//
+//  MainTabView.swift
+//  cafeSip
+//
+//  Created by hyunMac on 12/11/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @State var tabIndex = 0
+    
+    var body: some View {
+        TabView(selection: $tabIndex) {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                }
+                .tag(0)
+            Text("pay")
+                .tabItem {
+                    Image(systemName: "creditcard.fill")
+                }
+                .tag(1)
+            Text("order")
+                .tabItem {
+                    Image(systemName: "mug.fill")
+                }
+                .tag(2)
+            Text("other")
+                .tabItem {
+                    Image(systemName: "ellipsis")
+                }
+                .tag(3)
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
