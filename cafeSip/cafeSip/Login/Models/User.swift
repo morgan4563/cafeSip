7d435509271e915f1a94a7310aedfd8be32867d4
morgan4563 <morganpark4563@gmail.com>
2025-01-09 16:44:23 +0900

    feat: MVVM 패턴으로 변경을 위한 폴더 생성, User데이터를 저장할 User Model 생성

---
+++ cafeSip/cafeSip/Login/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  cafeSip
+//
+//  Created by hyunMac on 1/9/25.
+//
+
+import Foundation
+
+struct User {
+    var email: String
+    var password: String
+    var userName: String
+}
