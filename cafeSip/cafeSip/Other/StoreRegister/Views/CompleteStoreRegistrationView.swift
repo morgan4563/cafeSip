1eda3c25286c7125ca4712505a89cee3a2e4b4ca
morgan4563 <morganpark4563@gmail.com>
2025-01-13 05:03:18 +0900

    feat: StoreRegister뷰 텝바 조정, 네비게이션 백버튼 수정, CompleteStoreRegistrationView 구성

---
+++ cafeSip/cafeSip/Other/StoreRegister/Views/CompleteStoreRegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  CompleteStoreRegistrationView.swift
+//  cafeSip
+//
+//  Created by hyunMac on 1/12/25.
+//
+
+import SwiftUI
+
+struct CompleteStoreRegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompleteStoreRegistrationView()
+}
