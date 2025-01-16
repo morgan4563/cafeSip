9fbc0ad3dea1c275a4b7835ee564b0dcb319e45d
morgan4563 <morganpark4563@gmail.com>
2025-01-17 00:43:42 +0900

    feat: Order수정, 메뉴의 준비상태를 추가

---
+++ cafeSip/cafeSip/Order/OrderStatusView.swift
@@ -0,0 +1,19 @@
+//
+//  OrderStatusView.swift
+//  cafeSip
+//
+//  Created by hyunMac on 1/17/25.
+//
+
+import SwiftUI
+
+struct OrderStatusView: View {
+    var body: some View {
+        Text("메뉴가 준비중")
+        
+    }
+}
+
+#Preview {
+    OrderStatusView()
+}
