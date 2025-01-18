9d8db3322335e3bf73363518b2cf346b72c866a5
morgan4563 <morganpark4563@gmail.com>
2025-01-19 05:39:12 +0900

    refactor: AppModels 한 파일에서 관리하도록 변경

--- cafeSip/cafeSip/Models/AppModels.swift
+++ cafeSip/cafeSip/Models/AppModels.swift
@@ -17,3 +17,21 @@ struct Order: Identifiable, Codable {
     var menuName: String
     var price: String
 }
+
+struct Store: Codable {
+    let id: String
+    let ownerId: String
+    var name: String
+    let address: String
+    let detailAddress: String
+    var menuItems: [MenuItem]?
+}
+
+struct User: Codable {
+    let id: String
+    let email: String
+    var userName: String
+    var balance: Int
+    var storeId: String?
+}
+
