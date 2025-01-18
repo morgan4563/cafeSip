dc19f5e7b0633b0e777c9ca0215009533af24213
morgan4563 <morganpark4563@gmail.com>
2025-01-19 03:13:53 +0900

    feat: 앱 라이프사이클에 따른 체크 로직 추가, 추후 각 상황별 처리 로직 추가필요

--- cafeSip/cafeSip/cafeSipApp.swift
+++ cafeSip/cafeSip/cafeSipApp.swift
@@ -20,9 +20,23 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 @main
 struct cafeSipApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase) { oldPhase , newPhase in
+            switch newPhase {
+            case .active:
+                print("앱 활성상태")
+            case .inactive:
+                print("앱 비활성화 상태")
+            case .background:
+                print("앱 백그라운드 전환 상태")
+            default:
+                break
+            }
         }
     }
 }
